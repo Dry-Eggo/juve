fdaa3391764655fbdb3bb6ed31eac61b6f41ee78
Dry-Eggo <abdulewenyi3@gmail.com>
2025-08-03 14:35:04 +0100

    INit

--- juve_utils.h
+++ juve_utils.h
@@ -1,63 +1,252 @@
 #pragma once
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
 
+/**
+ * Juve Library
+ * Contains implementations for various utilities that i frequently use
+ **/
 
-/*
-* Juve Library
-* Contains implementations for various utilities that i frequently use
-* it is split into two versions:
-    * STL-Implementation: this version uses C++ stdlib to provide support for these utilites
-    * Pure Implementation: this comprises of the utilites that are implemented in pure C .
-**/
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 #include <stddef.h>
 #include <stdbool.h>
-//==------------------- STL-Implemntation -------------------===
 
-// JBuffer: A resizable buffer for string operations (non arena)
-// Uses std::stringstream under the hood
-typedef struct JBuffer JBuffer;
+#define JBUF_INI_CAP 255
+#define JARENA_INI_CAP 255
 
+#define MAX(a, b) ((a > b) ? a : b)
 
-// JArena: A basic allocation tracker.
-// stores allocations in one place ready to be torn down in one go
-// Uses std::vector<void*>
 typedef struct JArena JArena;
+struct JArena {
+    char* base;
+    size_t capacity, offset;
+};
 
-JBuffer*    jb_create(void);
-void        jb_append(JBuffer*, const char*);
+static inline JArena  jarena_new(void);
+static inline void*   jarena_alloc(JArena*, size_t);
+static inline void*   jarena_zeroed(JArena*, size_t);
+static inline void    jarena_free(JArena*);
+static inline void    jarena_reset(JArena*);
+static inline char*   jarena_strdup(JArena*, char*);
+
+#ifdef JUVE_IMPLEMENTATION
+static inline JArena jarena_new() {
+    JArena arena;
+    arena.capacity = JARENA_INI_CAP;
+    arena.offset   = 0;
+    arena.base     = (char*)malloc(arena.capacity);
+    return arena;
+}
+
+static inline void jarena_reset(JArena* arena) {
+    arena->offset = 0;
+}
+
+static inline void* jarena_alloc(JArena* arena, size_t size) {
+    const size_t align = 8;
+    size = (size + (align - 1)) & ~(align - 1);
+
+    if (arena->offset + size > arena->capacity) {
+        size_t new_size = MAX(arena->offset + size, arena->capacity*2);
+        arena->base = realloc(arena->base, new_size);
+        arena->capacity = new_size;
+    }
+    
+    void* ptr = arena->base + arena->offset;
+    arena->offset += size;
+    return ptr;
+}
+
+static inline void*  jarena_zeroed(JArena* arena, size_t size) {
+    void* ptr = jarena_alloc(arena, size);
+    if (ptr) memset(ptr, 0, size);
+    return ptr;
+}
+
+static inline char* jarena_strdup(JArena* arena, char* str) {
+    size_t len = strlen(str) + 1;
+    char* copy = (char*)jarena_alloc(arena, len);
+    if (copy) memcpy(copy, str, len);
+    return copy;
+}
+#endif
+
+
+// JBuffer: A resizable buffer for string operations
+typedef struct JBuffer JBuffer;
+struct JBuffer {
+    char* data;
+    size_t len;
+    size_t cap;
+
+    JArena* arena;
+};
+
+JBuffer     jb_create(JArena*);
+size_t        jb_append(JBuffer*, const char*);
 void        jb_appendf_a(JBuffer*, JArena*, const char* fmt, ...);
-void        jb_print(JBuffer*);
-char*       jb_str(JBuffer*);
-char*       jb_str_a(JBuffer* jb, JArena* arena);
+void        jb_print(JBuffer);
+char*       jb_str(JBuffer);
+char*       jb_str_a(JBuffer jb, JArena* arena);
 void        jb_free(JBuffer*);
 void        jb_clear(JBuffer*);
-bool        jb_eq(JBuffer*, const char*);
-size_t      jb_len(JBuffer*);
+bool        jb_eq(JBuffer, const char*);
+size_t      jb_len(JBuffer);
 
-void*    jmalloc(size_t);
-void     jfree(void*);
+#ifdef JUVE_IMPLEMENTATION
+JBuffer jb_create(JArena* arena) {
+    return (JBuffer) {
+        .len = 0,
+        .cap = JBUF_INI_CAP,
+        .data = jarena_alloc(arena, sizeof(char)*JBUF_INI_CAP),
+        .arena = arena
+    };
+}
 
-JArena*  jarena_new(void);
-void*    jarena_alloc(JArena*, size_t);
-void*    jarena_zeroed(JArena*, size_t);
-void     jarena_free(JArena*);
-void     jarena_reset(JArena*);
-char*    jarena_strdup(JArena*, char*);
+JBuffer jb_from_str(JArena* arena, const char* cstr) {
+    JBuffer buf = jb_create(arena);
+    jb_append(&buf, cstr);
+    return buf;
+}
 
-// file utils
-typedef struct CJBuffer CJBuffer;
-size_t      jb_read_entire_file(const char*, CJBuffer*);
-bool        jfile_write(const char*, const char*);
-char*       jfile_stem(const char*, JArena*);
-char*       jfile_ext(const char*, JArena*);
-char*       jfile_dirname(const char*, JArena*);
-char*       jfile_basename(const char*, JArena*);
-bool        jfile_exists(const char*);
-long        jfile_size(const char*);
+size_t jb_append(JBuffer* buf, const char* str) {
+    if (strlen(str) == 0) return 0;
+    
+    size_t total_len = buf->len + strlen(str);
+    if (total_len >= buf->cap) {
+        size_t newsz = total_len > buf->cap*2 ? total_len : buf->cap*2;
+        char* new_data = (char*)jarena_alloc(buf->arena, newsz);
+        if (!new_data) return -1;      
+        
+        memcpy(new_data, buf->data, sizeof(char)*buf->len);
+        buf->data = new_data;
+        buf->cap = newsz;
+    }
+
+    memcpy(buf->data + buf->len, str, strlen(str) + 1);
+    buf->len =  total_len;
+    return total_len;
+}
+
+size_t jb_appendf(JBuffer* buf, const char* fmt, ...) {
+    if (strlen(fmt) == 0) return 0;
+
+    va_list args;
+    va_start(args, fmt);
+
+    size_t needed = vsnprintf(NULL, 0, fmt, args);
+
+    char* str = (char*)jarena_alloc(buf->arena, needed + 1);
+    va_list args_real;
+    va_start(args_real, fmt);
+
+    vsnprintf(str, needed + 1, fmt, args_real);
+
+    jb_append(buf, str);
+    
+    return 1;
+}
+
+void  jb_print(JBuffer buf) {
+    printf("%s", buf.data);
+}
+
+char* jb_str(JBuffer buf) {
+    return jarena_strdup(buf.arena, buf.data);
+}
+
+void  jb_clear(JBuffer* buf) { buf->len = 0; buf->data[0] = '\0'; }
+bool  jb_eq(JBuffer buf, const char* str) { return strcmp(buf.data, str) == 0; }
+size_t jb_len(JBuffer buf) { return buf.len; }
+#endif
+
+
+size_t jb_read_entire_file(const char*, JBuffer*);
+bool   jfile_write(const char*, const char*);
+char*  jfile_stem(const char*, JArena*);
+char*  jfile_ext(const char*, JArena*);
+char*  jfile_dirname(const char*, JArena*);
+char*  jfile_basename(const char*, JArena*);
+bool   jfile_exists(const char*);
+size_t jfile_size(const char*);
+
+
+#ifdef JUVE_IMPLEMENTATION
+size_t jfile_size(const char* path) {
+    FILE* file = fopen(path, "r");
+    if (!file) return -1;
+
+    fseek(file, 0, SEEK_END);
+    size_t size = ftell(file);
+    
+    fclose(file);
+    return size;
+}
+
+size_t jb_read_entire_file(const char* path, JBuffer* buffer) {
+    size_t size = jfile_size(path);
+    if (size == -1) return -1;
+
+    FILE* file = fopen(path, "rb");
+    char* content = (char*)jarena_alloc(buffer->arena, size + 1);
+    if (!content) return -1;
+    
+    fread(content, 1, size, file);
+    if (!content) return -1;
+
+    jb_append(buffer, content);
+    
+    fclose(file);
+    return size;
+}
+
+bool jfile_write(const char* path, const char* content) {
+    FILE* file = fopen(path, "wb");
+    if (!file) return false;
+
+    fprintf(file, "%s", content);
+    fclose(file);
+    return true;
+}
+
+char* jfile_stem(const char* path, JArena* arena) {
+    const char* path_ = path;
+    const char* last_slash = strrchr(path, '/');
+
+    if (last_slash) {
+        path_ = last_slash + 1;
+    }
+
+    const char* last_dot = strrchr(path_, '.');
+    size_t len = last_dot ? (size_t) (last_dot - path_) : strlen(path_);
+    char* stem = (char*)jarena_alloc(arena, len + 1);
+
+    if (!stem) return NULL;
+    memcpy(stem, path_, len);
+    stem[len] = '\0';
+    return stem;
+}
+
+char* jfile_ext(const char* path, JArena* arena) {
+    const char* filename = path;
+    const char* last_dot = strrchr(path, '.');
+
+    if (last_dot) {
+        char* ext = jarena_alloc(arena, strlen(last_dot));
+        memcpy(ext, last_dot + 1, strlen(last_dot));
+        return ext;
+    }
+    return NULL;
+}
+
+char* jfile_dirname(const char* path, JArena* arena) {
+    
+}
+
+#endif
+
 
 // containers
 typedef struct JVec JVec;
--- test.c
+++ test.c
@@ -1,6 +1,7 @@
+#define JUVE_IMPLEMENTATION
 #include "juve_utils.h"
 
-
 int main(void) {
-    
+    JArena arena = jarena_new();
+    printf("%s\n", jfile_ext("/home/dry/programming/c/cog/build.py", &arena));
 }
